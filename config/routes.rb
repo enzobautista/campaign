54d11cec170b966160cdce15d873bc2c207d7b0e
Lorenzo Rafael T. Bautista <lorenzo.bautista@obf.ateneo.edu>
2016-04-21 03:29:12 +0800

    precompiled

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :messages
   get 'pages/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -6,7 +7,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'pages#index'
+  root 'messages#new'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
